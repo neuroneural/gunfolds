a35977ef199c8bca876fdaaab6cc7de837813056
Girish <79399756+Girish-Anadv-07@users.noreply.github.com>
2023-10-25 01:15:26 -0400

    Update version_update.sh

--- version_update.sh
+++ version_update.sh
@@ -5,15 +5,15 @@ sed -i -e "1s/s = .*/s = [$version]/" version_update.py
 git checkout -b test1
 git add version_update.py
 git commit -m "version update"
-git push origin test1
+git push -f origin test1
 
 updated_version=$(python version_update.py)
 sed -i -e "1s/__version_info__ = .*/__version_info__ = $updated_version/" version.py
 git add version.py
 git commit -m "version update"
-git push origin test1
+git push -f origin test1
 git checkout master
 git pull origin master
 git merge test1
-git push origin master
+git push -f origin master
 rm *.py-e
