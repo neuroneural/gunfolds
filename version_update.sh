451667a4c74c0633ddd236b0325750c8cdcc1a95
Girish <79399756+Girish-Anadv-07@users.noreply.github.com>
2023-10-26 18:54:49 -0400

    Create version_update.sh

---
+++ version_update.sh
@@ -0,0 +1,15 @@
+version=`cat version.txt`
+
+sed -i -e "1s/s = .*/s = $version/" version_update.py
+rm *.py-e
+
+updated_version=$(python version_update.py)
+echo $updated_version > version.txt
+
+git add version.txt version_update.py
+git commit -m "version update to $updated_version"
+git push origin version
+git checkout master
+
+sed -i -e "1s/__version_info__ = .*/__version_info__ = $updated_version/" version.py
+rm *.py-e
