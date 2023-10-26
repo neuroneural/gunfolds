a845de4e7512421b62eab3d2f9990cebd64b1084
Girish <79399756+Girish-Anadv-07@users.noreply.github.com>
2023-10-26 18:55:56 -0400

    Create version_update.py

---
+++ version_update.py
@@ -0,0 +1,17 @@
+s = [0, 0, 7]
+v = []
+for i in range(0,101):
+    v.append(i)
+if s[2]==99:
+    s[2]=0
+    if s[1]==99:
+        s[1]=0
+        if s[0]==99:
+            s[0]=0
+        else:
+            s[0]=v[s[0]+1]
+    else:
+        s[1]=v[s[1]+1]
+else:
+    s[2]=v[s[2]+1]
+print(s)
